5e055c77ff9d0e99495db5e0eec0060376bbbc58
JuanCarlosSotoGarcia <carloscc1125@gmail.com>
2025-04-06 16:14:40 -0600

    main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #define LINKED_LIST_IMPLEMENTATION
 #include "linkedlist.h"
 #include <stdio.h>
+#include <stdlib.h>
 
+// Funciones para imprimir y comparar enteros
 void print_int(int value) {
     printf("%d ", value);
 }
@@ -10,6 +12,7 @@ bool int_cmp(int a, int b) {
     return a == b;
 }
 
+// Funciones para imprimir y comparar caracteres
 void print_char(char value) {
     printf("%c ", value);
 }
@@ -19,27 +22,49 @@ bool char_cmp(char a, char b) {
 }
 
 int main() {
-    List_int* nums = list_int_create();
-    list_int_append(nums, 10);
-    list_int_append(nums, 20);
+    // Crear y usar una lista ligada de enteros
+    List* nums = list_create(free); 
+    int* num1 = malloc(sizeof(int));
+    *num1 = 10;
+    list_append(nums, num1);
+    
+    int* num2 = malloc(sizeof(int));
+    *num2 = 20;
+    list_append(nums, num2);
     
     printf("Lista de enteros: ");
-    list_int_print(nums, print_int);
+    void* data;
+    for (size_t i = 0; i < list_length(nums); i++) {
+        if (list_get(nums, i, &data)) {
+            print_int(*(int*)data);
+        }
+    }
     printf("\n");
-
-    list_int_destroy(nums);
     
-    List_char *chars = list_char_create();
-    list_char_append(chars, 'a');
-    list_char_append(chars, 'b');
-    list_char_append(chars, 'c');
-    list_char_append(chars, 'd');
-    list_char_append(chars, 'e');
+    list_destroy(nums);
+
+    // Crear y usar una lista ligada de caracteres
+    CircularList* chars = circular_list_create(free);
+    char* char1 = malloc(sizeof(char));
+    *char1 = 'a';
+    circular_list_append(chars, char1);
+    
+    char* char2 = malloc(sizeof(char));
+    *char2 = 'b';
+    circular_list_append(chars, char2);
+    
+    char* char3 = malloc(sizeof(char));
+    *char3 = 'c';
+    circular_list_append(chars, char3);
 
     printf("Lista de caracteres: ");
-    list_char_print(chars, print_char);
+    for (size_t i = 0; i < circular_list_length(chars); i++) {
+        if (circular_list_get(chars, i, (void**)&data)) {
+            print_char(*(char*)data);
+        }
+    }
     printf("\n");
 
-    list_char_destroy(chars);
+    circular_list_destroy(chars);
     return 0;
 }
